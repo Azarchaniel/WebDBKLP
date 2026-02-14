83e85c17188e14d339565f063f927a0c5ce33f36
Azarchaniel <csonkalubos@gmail.com>
2026-02-14 19:42:11 +0100

    Update packages and configs (#26)

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,4 +1,4 @@
-const { override, addWebpackAlias } = require('customize-cra');
+const { override, addWebpackAlias, adjustStyleLoaders } = require('customize-cra');
 const path = require('path');
 
 module.exports = override(
@@ -7,5 +7,14 @@ module.exports = override(
         '@components': path.resolve(__dirname, 'src/components'),
         '@utils': path.resolve(__dirname, 'src/utils'),
         '@hooks': path.resolve(__dirname, 'src/utils/hooks/index.ts')
+    }),
+    adjustStyleLoaders(({ use: [, , postcss, , sass] }) => {
+        // Configure sass-loader to use modern API
+        if (sass) {
+            sass.options = {
+                ...sass.options,
+                api: 'modern'
+            };
+        }
     })
 );
